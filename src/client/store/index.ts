7f0625e47b2da437a8660270b790e0f08f0ac90e
Max Ustinov <ewclide@yandex.ru>
2024-06-07 14:54:57 +0400

    install mobx & add store

---
+++ src/client/store/index.ts
@@ -0,0 +1,43 @@
+import { makeAutoObservable } from "mobx";
+import { roundInt } from "../utils";
+
+export class GameStore {
+    lifes: number = 5;
+    money: number = 0;
+    distance: number = 0;
+    time: number = 0;
+
+    static createMobXStore(): GameStore {
+        return makeAutoObservable(new GameStore());
+    }
+
+    get distance_km(): number {
+        return Math.floor(this.distance / 1000);
+    }
+
+    get distance_m(): number {
+        return roundInt(Math.floor(this.distance % 1000), 10);
+    }
+
+    get seconds(): number {
+        return this.time;
+    } 
+
+    increaseLife(): void {
+        this.lifes++;
+    }
+
+    addMoney(money: number) {
+        this.money += money;
+    }
+
+    addDistance(delta: number) {
+        this.distance += delta;
+    }
+
+    addTime(delta: number) {
+        this.time += delta;
+    }
+}
+
+
